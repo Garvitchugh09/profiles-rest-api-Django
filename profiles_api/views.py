45712656426ab0043a7492e930437cede51cba64
Garvit <chughg412@gmail.com>
2024-08-31 16:33:43 +0530

    Added get and post methods to api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API view"""
+    serializer_class = serializers.HelloSerializer
     def get(self,request,format = None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -12,3 +15,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Success', 'an_apiview': an_apiview})
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST)
