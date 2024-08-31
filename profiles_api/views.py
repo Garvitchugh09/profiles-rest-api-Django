551ff1236a36d50a1a317c011f165e654a24e1f1
Garvit <chughg412@gmail.com>
2024-08-31 21:39:03 +0530

    Added viewset to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -24,6 +25,28 @@ class HelloApiView(APIView):
             message = f'Hello {name}'
             return Response({'message':message})
 
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST)
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        a_viewset = [
+        'Uses actions (list, create, retrive, update, partial_uodate)',
+        'Automatically maps to URLs using routers',
+        'Provides more functionality with lessc code'
+        ]
+        return Response({'message': 'Hello', 'viewset': a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name} !'
+            return Response({'message':message})
         else:
             return Response(
                 serializer.errors,
