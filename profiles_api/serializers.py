f382e863273cb7bf42011375831d0f7957797256
Garvit <chughg412@gmail.com>
2024-09-06 16:03:05 +0530

    Adding profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,4 +1,31 @@
 from rest_framework import serializers
+from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length = 10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id','email','name','password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type':'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+        email = validated_data['email'],
+        name = validated_data['name'],
+        password = validated_data['password']
+        )
+        return user
+        
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API view"""
@@ -51,3 +54,9 @@ class HelloViewSet(viewsets.ViewSet):
             return Response(
                 serializer.errors,
                 status = status.HTTP_400_BAD_REQUEST)
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
